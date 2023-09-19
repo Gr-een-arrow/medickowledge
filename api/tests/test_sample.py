09447547dc5446f9d4b76cdf4b14a911a9c8b91f
Gr-een-arrow <ahamedmk2001@gmail.com>
2023-09-19 14:33:59 +0530

    test sample

---
+++ api/tests/test_sample.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class TestSample(TestCase):
+    def test_sample(self):
+        self.assertEqual(1, 1)
+
+    def test_sample1(self):
+        self.assertTrue(True)
